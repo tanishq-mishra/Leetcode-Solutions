40bd61744510899f82f8533f8ad303767aba8971
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-02-21 01:17:49 +0530

    Time: 1 ms (91.93%), Space: 41 MB (25.96%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -10,14 +10,29 @@ class Solution {
         
         return product;
     }
-    public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
+//     public boolean isHappy(int n) {
+//         Set<Integer> seen = new HashSet<>();
         
-        while(n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getProduct(n);
+//         while(n != 1 && !seen.contains(n)) {
+//             seen.add(n);
+//             n = getProduct(n);
+//         }
+        
+//         return n == 1;
+//     }
+    
+    
+    // Using Fast and Slow Pointer Method
+    
+    public boolean isHappy (int n) {
+        int fast = n;
+        int slow = n;
+        do {
+            fast = getProduct(getProduct(fast));
+            slow = getProduct(slow);
         }
+        while(fast != slow);
         
-        return n == 1;
+        return slow == 1;
     }
 }

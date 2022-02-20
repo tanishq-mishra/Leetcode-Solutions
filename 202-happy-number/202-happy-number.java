f983e4f257ad9355706307f0f2f70c5482c8ecde
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-02-21 01:07:52 +0530

    Time: 5 ms (10.77%), Space: 41.6 MB (10.73%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,31 +1,38 @@
 class Solution {
     
-    public ArrayList<Integer> getDigits(int n) {
-        int numberOfDigits = (int)Math.floor(Math.log10(n) + 1);
-        ArrayList<Integer> digits = new ArrayList<Integer>();
-        int i =0;
-        while(i < numberOfDigits) {
-            int temp = n % 10;
-            n = n/10;
-            digits.add(temp);
-            i++;
+//     public ArrayList<Integer> getDigits(int n) {
+//         int numberOfDigits = (int)Math.floor(Math.log10(n) + 1);
+//         ArrayList<Integer> digits = new ArrayList<Integer>();
+//         int i =0;
+//         while(i < numberOfDigits) {
+//             int temp = n % 10;
+//             n = n/10;
+//             digits.add(temp);
+//             i++;
             
+//         }
+        
+//         return digits;
+//     }
+    
+    public int getProduct(int n) {
+        int product = 0;
+        
+        while(n > 0) {
+            int temp = n %10;
+            product = product + (temp*temp);
+            n = n/10;
         }
         
-        return digits;
+        return product;
     }
     public boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
         
         while(n != 1) {
-            ArrayList<Integer> digits = getDigits(n);
+            //ArrayList<Integer> digits = getDigits(n);
             //System.out.println(digits);
-            int product = 0;
-            
-            for(int d: digits) {
-                product = product + (d*d);
-                
-            }
+            int product = getProduct(n);
             //System.out.println(product);
             if(product == 1) {
                 return true;

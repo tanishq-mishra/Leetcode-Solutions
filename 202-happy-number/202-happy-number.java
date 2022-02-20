28fc604dec9c5fceebc8fa20ef7f1fada4a8a26d
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-02-21 01:02:27 +0530

    Time: 4 ms (18.97%), Space: 42.2 MB (5.12%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public ArrayList<Integer> getDigits(int n) {
+        int numberOfDigits = (int)Math.floor(Math.log10(n) + 1);
+        ArrayList<Integer> digits = new ArrayList<Integer>();
+        int i =0;
+        while(i < numberOfDigits) {
+            int temp = n % 10;
+            n = n/10;
+            digits.add(temp);
+            i++;
+            
+        }
+        
+        return digits;
+    }
+    public boolean isHappy(int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        while(n != 1) {
+            ArrayList<Integer> digits = getDigits(n);
+            //System.out.println(digits);
+            int product = 0;
+            
+            for(int d: digits) {
+                product = product + (d*d);
+                
+            }
+            //System.out.println(product);
+            if(product == 1) {
+                return true;
+            }
+            
+            if(map.containsKey(product)) {
+                return false;
+            }
+            map.put(product, 1);
+            
+            n = product;
+        }
+        
+        return true;
+    }
+}

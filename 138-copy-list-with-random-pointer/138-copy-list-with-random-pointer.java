252b37119deda87ac7f8257b539a7b4f13017533
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-02-23 13:23:25 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (18.75%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -1,78 +1,26 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
-    public int findRandom(Node node,  Node random) {
-        if(random == null) {
-            return -1;
-        }
-        int i = 0;
-        while(node != random) {
-            node = node.next;
-            i++;
-        }
-        
-        return i;
-    }
-    
-    public Node findNode(Node head, int index) {
-        while(index > 0) {
-            head = head.next;
-            index--;
-        }
-        
-        return head;
-    } 
     public Node copyRandomList(Node head) {
-        if(head == null) {
+        if(head==null)
             return null;
+        HashMap<Node,Node> hm=new HashMap<>();
+        Node temp=head;
+        Node newHead= null;
+        while(temp!=null)
+        {
+            Node copyNode = new Node(temp.val);
+            hm.put(temp,copyNode);
+            temp=temp.next;
         }
-        
-       
-        Node cloneHead = new Node(head.val);
-        Node tail = head;
-        Node cloneTail = cloneHead;
-        tail = tail.next;
-        int length = 1;
-        
-        while(tail != null) {
-            length++;
-            cloneTail.next = new Node(tail.val);
-            tail = tail.next;
-            cloneTail = cloneTail.next;
+        temp=head;
+        while(temp!=null)
+        {
+            Node copyNode=hm.get(temp);
+            copyNode.random=hm.get(temp.random);
+            copyNode.next=hm.get(temp.next);
+            if(newHead==null)
+                newHead=copyNode;
+            temp=temp.next;
         }
-        tail = head;
-        cloneTail = cloneHead;
-        int i = 0;
-        while(i<length) {
-            int randomIndex = findRandom(head, tail.random);
-            
-            if(randomIndex == -1) {
-                cloneTail.random = null;
-            } else {
-                Node randomNode = findNode(cloneHead, randomIndex);
-                cloneTail.random = randomNode;
-            }
-            
-            tail = tail.next;
-            cloneTail = cloneTail.next;
-            i++;
-        }
-       
-        
-        return cloneHead;
-        
+        return newHead;
     }
 }

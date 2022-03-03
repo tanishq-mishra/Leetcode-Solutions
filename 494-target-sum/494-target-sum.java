a1b256eec45248d4550004be5ce6fe46742d388f
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-03 15:32:10 +0530

    Time: 268 ms (45.72%), Space: 72.1 MB (10.46%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    HashMap<String,Integer> memo = new HashMap<>();
+    public int helper(int[] nums, int target, int index, int sum) {
+        String memoKey = sum + "," + index;
+        if(memo.containsKey(memoKey)) {
+            return memo.get(memoKey);
+        }
+        if(index == nums.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        int fromBelowCalls = 0;
+        fromBelowCalls +=helper(nums, target, index+1 , sum + nums[index]);
+        fromBelowCalls += helper(nums, target, index+1, sum - nums[index]);
+        memo.put(memoKey, fromBelowCalls);
+        return fromBelowCalls;
+    }
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        return helper(nums, target, 0, 0);
+    }
+}

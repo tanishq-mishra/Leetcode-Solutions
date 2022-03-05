eb15fabab21eb4b870db05d5e8acfc35784193b4
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 00:41:55 +0530

    Time: 0 ms (100.00%), Space: 46.3 MB (32.09%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length;
+        int mid = start + (end - start)/2;
+        
+        while(start < end) {
+            mid = start + (end - start)/2;
+            if(arr[mid] < arr[mid + 1]) {
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return start;
+    }
+}

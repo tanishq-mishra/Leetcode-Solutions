76ee455d1fd0fe8a29e0badf812a63585cb9ae88
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-05 17:39:29 +0530

    Time: 7 ms (36.57%), Space: 50.7 MB (36.67%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,21 +1,35 @@
 class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
+        
+//         for(int i = 0; i<nums.length; i++) {
+//             if(count.containsKey(nums[i])) {
+//                 count.put(nums[i], count.get(nums[i]) + 1 );
+//             } else {
+//                 count.put(nums[i], 1);
+//             }
+//         }
+        
+//         for(Map.Entry<Integer,Integer> entry : count.entrySet()) {
+//             if(entry.getValue() == 1) {
+//                 return entry.getKey();
+//             }
+//         }
+        
+//         return -1;
+//     }
+    
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
         
-        for(int i = 0; i<nums.length; i++) {
-            if(count.containsKey(nums[i])) {
-                count.put(nums[i], count.get(nums[i]) + 1 );
-            } else {
-                count.put(nums[i], 1);
+        for(int i = 0; i< nums.length-1; i+=2) {
+            if(nums[i] != nums[i+1]) {
+                return nums[i];
             }
         }
-        
-        for(Map.Entry<Integer,Integer> entry : count.entrySet()) {
-            if(entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-        
-        return -1;
+        return nums[nums.length -1];
     }
 }

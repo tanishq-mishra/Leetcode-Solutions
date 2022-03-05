d25dcf57c2a5cde3794597fa989903de64eafc91
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 03:24:36 +0530

    Time: 17 ms (5.22%), Space: 42.1 MB (6.61%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,29 @@
+class Solution {
+    public HashMap<String, Integer> memo = new HashMap<>();
+    public int helper(int x, int y, int m, int n) {
+        String id = x + "," + y;
+        if(memo.containsKey(id)) {
+            return memo.get(id);
+        }
+        if(x == m && y == n) {
+            return 1;
+        }
+        if(x>m) {
+            return 0;
+        }
+        if(y>n) {
+            return 0;
+        }
+        
+        int sum = 0;
+        
+        sum += helper(x+1, y, m, n);
+        sum += helper(x, y+1, m, n);
+        memo.put(id, sum);
+        return sum;
+        
+    }
+    public int uniquePaths(int m, int n) {
+        return helper(1,1, m, n);    
+    }
+}

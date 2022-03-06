4e81c59f80db105cc5dc02bf3ccbdbba3c5c18ab
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 22:10:39 +0530

    Time: 13 ms (60.71%), Space: 55.6 MB (11.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -4,32 +4,33 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
         ArrayList<int[]> inputList = new ArrayList<>();
-        for(int i = 0 ; i< intervals.length; i++) {
-            int [] newInterval = new int [2];
-            newInterval[0] = (intervals[i][0]);
-            newInterval[1] = (intervals[i][1]);
-            inputList.add(newInterval);
-        }  
-        int i = 0;
-        while(i< inputList.size()) {
-            if(i < inputList.size()-1 && inputList.get(i+1)[0] <= inputList.get(i)[1]) {
-                int [] newInterval = new int [2];
-                newInterval[0] = inputList.get(i)[0];
-                newInterval[1] = Math.max(inputList.get(i)[1],inputList.get(i+1)[1]);
-                inputList.set(i, newInterval);
-                inputList.remove(i+1);
+        // for(int i = 0 ; i< intervals.length; i++) {
+        //     int [] newInterval = new int [2];
+        //     newInterval[0] = (intervals[i][0]);
+        //     newInterval[1] = (intervals[i][1]);
+        //     inputList.add(newInterval);
+        // }  
+        int[] newInterval = intervals[0];
+        inputList.add(newInterval);
+        for(int[] in: intervals) {
+            if(in[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(in[1],newInterval[1]);
             } else {
-                i++;
+                newInterval = in;
+                inputList.add(newInterval);
             }
         }
-        
-        // int[][] ans = new int[inputList.size()][2];
-        // i = 0;
-        // for(ArrayList<Integer> a: inputList) {
-        //     ans[i][0] = a.get(0);
-        //     ans[i++][1] = a.get(1);
+        // while(i< inputList.size()) {
+        //     if(i < inputList.size()-1 && inputList.get(i+1)[0] <= inputList.get(i)[1]) {
+        //         int [] newInterval = new int [2];
+        //         newInterval[0] = inputList.get(i)[0];
+        //         newInterval[1] = Math.max(inputList.get(i)[1],inputList.get(i+1)[1]);
+        //         inputList.set(i, newInterval);
+        //         inputList.remove(i+1);
+        //     } else {
+        //         i++;
+        //     }
         // }
-        
         return  inputList.toArray(new int[inputList.size()][]);
     }
 }

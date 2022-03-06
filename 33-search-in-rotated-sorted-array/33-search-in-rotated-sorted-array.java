3c6f6abff3bdfb229a9a69ace3dcedda1a5df790
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 14:09:08 +0530

    Time: 1 ms (66.48%), Space: 42.9 MB (19.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -6,34 +6,19 @@ class Solution {
         
         while(start <= end) {
             mid = start + (end - start)/2; 
-            //System.out.println(start + ","+ mid+ ","+ end);
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[start] == target) {
-                return start;
-            } else if(nums[end] == target) {
-                return end;
-            }
-            else {
-                if(nums[start] <= nums[mid]) {
-                    
-                    if(nums[start] < target && nums[mid] > target) {
-                        end = mid-1;
-                    } else {
-                        start = mid+1;
-                    }
-                } else {
-                    
-                    if(nums[start] > target && nums[mid] < target) {
-                        start = mid+1;
-                    } else if (nums[start] > target && nums[mid] > target) {
-                        end = mid-1;
-                    }else {
-                        end = mid-1;
-                    }
-                    
-                }
-            }
+            if (nums[mid] == target)
+            return mid;
+             if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid])
+                  end = mid - 1;
+                else
+                  start = mid + 1;
+              } else {
+                if (target <= nums[end] && target > nums[mid])
+                  start = mid + 1;
+                else
+                  end = mid - 1;
+              }
         }
         
         return -1;

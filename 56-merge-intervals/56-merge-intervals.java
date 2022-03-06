1d8285dfea14f10f91b0e97f40b8507425b5ebbd
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 22:14:14 +0530

    Time: 10 ms (78.82%), Space: 54.1 MB (47.29%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -2,7 +2,7 @@
 import java.util.*;   
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
         ArrayList<int[]> inputList = new ArrayList<>();
         
         int[] newInterval = intervals[0];

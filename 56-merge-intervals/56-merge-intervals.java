1646a2d9ce2367b3a5d50feef7c3bbbcd9366dd1
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-06 22:11:07 +0530

    Time: 14 ms (54.36%), Space: 55.6 MB (11.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -4,12 +4,7 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
         ArrayList<int[]> inputList = new ArrayList<>();
-        // for(int i = 0 ; i< intervals.length; i++) {
-        //     int [] newInterval = new int [2];
-        //     newInterval[0] = (intervals[i][0]);
-        //     newInterval[1] = (intervals[i][1]);
-        //     inputList.add(newInterval);
-        // }  
+        
         int[] newInterval = intervals[0];
         inputList.add(newInterval);
         for(int[] in: intervals) {
@@ -20,17 +15,7 @@ class Solution {
                 inputList.add(newInterval);
             }
         }
-        // while(i< inputList.size()) {
-        //     if(i < inputList.size()-1 && inputList.get(i+1)[0] <= inputList.get(i)[1]) {
-        //         int [] newInterval = new int [2];
-        //         newInterval[0] = inputList.get(i)[0];
-        //         newInterval[1] = Math.max(inputList.get(i)[1],inputList.get(i+1)[1]);
-        //         inputList.set(i, newInterval);
-        //         inputList.remove(i+1);
-        //     } else {
-        //         i++;
-        //     }
-        // }
+        
         return  inputList.toArray(new int[inputList.size()][]);
     }
 }

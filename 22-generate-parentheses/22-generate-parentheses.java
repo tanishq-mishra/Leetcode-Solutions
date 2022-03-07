6ea84eb7e50a504f5a7a310f799ae739fc05a346
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-07 20:39:44 +0530

    Time: 4 ms (27.52%), Space: 43.9 MB (24.34%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,33 +1,20 @@
 class Solution {
-    HashMap<String, HashSet<String>> memo = new HashMap<>();
-    public HashSet<String> helper(String p , int n) {
-        String key = p +", "+ n;
-        if(memo.containsKey(key)) {
-            return memo.get(key);
-        }
-        
-        if(n == 0) {
-            HashSet<String> ans = new HashSet<>();
-            ans.add(p);
-            return ans;
-        }
-        
-        HashSet<String> fromBelowCall = new HashSet<>();
-        
-        if(p.length() == 0) {
-            fromBelowCall.addAll(helper("()", n-1));
-        } else {
-            for(int i = 0; i< p.length(); i++) {
-            fromBelowCall.addAll(helper(p.substring(0,i) + "()" + p.substring(i), n-1));
-            }  
-            
-        }
-        memo.put(key, fromBelowCall);
-        
-        return fromBelowCall;
-    }
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>(helper("",n));
-        return ans;
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+        
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
     }
 }

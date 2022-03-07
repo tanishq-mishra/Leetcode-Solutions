aab3027ec8c602ecdc1bd893df7c5abe9aaf3f37
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-08 00:01:51 +0530

    Time: 4 ms (50.36%), Space: 56.5 MB (23.95%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,24 +1,46 @@
+
+//HashMap
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer, Integer> st = new HashMap<>();
+        
+//         for(int i: nums) {
+//             if(st.containsKey(i)) {
+//                 st.put(i, st.get(i) +1);
+//             } else {
+//                 st.put(i, 1);
+//             }
+//         }
+        
+//         for(Map.Entry<Integer,Integer> entry : st.entrySet()) {
+//             if(entry.getValue() > nums.length/2) {
+//                 return entry.getKey();
+//             }
+//         }
+        
+//         return -1;
+        
+        
+        
+//     }
+// }
+
+
+// Boyer-Moore Voting Algorithm
+
+
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> st = new HashMap<>();
-        
-        for(int i: nums) {
-            if(st.containsKey(i)) {
-                st.put(i, st.get(i) +1);
-            } else {
-                st.put(i, 1);
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
+            count += (num == candidate) ? 1 : -1;
         }
-        
-        for(Map.Entry<Integer,Integer> entry : st.entrySet()) {
-            if(entry.getValue() > nums.length/2) {
-                return entry.getKey();
-            }
-        }
-        
-        return -1;
-        
-        
-        
+
+        return candidate;
     }
 }

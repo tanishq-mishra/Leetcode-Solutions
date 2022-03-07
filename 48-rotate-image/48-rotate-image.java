f37d2b561fe31be3cb7f2e9c56ec03cb0a2e459e
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-07 23:24:57 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (5.90%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,13 +1,11 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int i = 0;
-        int x = 0;
         int size = matrix.length;
         while(i<=matrix.length/2) {
             int j = i;
             
-            while(j<matrix.length-x-1) {
-                //System.out.println(j);
+            while(j<matrix.length-i-1) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[size-j-1][i];
                 matrix[size-j-1][i] = matrix[size-i-1][size-j-1];
@@ -16,7 +14,6 @@ class Solution {
                 j++;
             }
             
-            x++;
             i++;
         }
     }

e771bd2eab6bb229385cba80ef07e2dd41438623
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-07 20:35:14 +0530

    Time: 46 ms (5.38%), Space: 50.2 MB (5.11%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,5 +1,11 @@
 class Solution {
+    HashMap<String, HashSet<String>> memo = new HashMap<>();
     public HashSet<String> helper(String p , int n) {
+        String key = p +", "+ n;
+        if(memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        
         if(n == 0) {
             HashSet<String> ans = new HashSet<>();
             ans.add(p);
@@ -12,12 +18,11 @@ class Solution {
             fromBelowCall.addAll(helper("()", n-1));
         } else {
             for(int i = 0; i< p.length(); i++) {
-            
             fromBelowCall.addAll(helper(p.substring(0,i) + "()" + p.substring(i), n-1));
             }  
             
         }
-        
+        memo.put(key, fromBelowCall);
         
         return fromBelowCall;
     }

df5364ae116b7cf602ac9681e492b97bef58ad24
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-09 15:23:33 +0530

    Time: 3 ms (99.29%), Space: 43.9 MB (87.66%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,16 +1,30 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] A, int k, int x) {
-        int left = 0;
-        int right = A.length - k;
-        
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (x - A[mid] > A[mid + k] - x)
-                left = mid + 1;
-            else
-                right = mid;
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        // find the closest element and expand the range
+        int closestIdx = 0;
+        int i = 0;
+        int j = arr.length - 1;
+        while(i < j - 1){
+            int mid = i + (j - i)/2;
+            if(arr[mid] > x) j = mid;
+            else i = mid;
         }
-        
-        return Arrays.stream(A, left, left + k).boxed().collect(Collectors.toList());
+        // post-processing
+        closestIdx = Math.abs(arr[i] - x) <= Math.abs(arr[j] - x) ? i : j;
+
+        List<Integer> list = new ArrayList<>();
+        int count = 1;
+        i = closestIdx - 1;
+        j = closestIdx + 1;
+        while(count < k){
+            if(i < 0) j++;
+            else if(j >= arr.length) i--;
+            else if(Math.abs(arr[i] - x) <= Math.abs(arr[j] - x)) i--;
+            else j++;
+            count++;
+        }
+                    
+        for(int m = i + 1; m <= j - 1; m++) list.add(arr[m]);
+        return list;
     }
 }

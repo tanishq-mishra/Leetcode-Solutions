d1b77b38250cf9ed430bcf8f3b0bb3530ac0f00c
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-09 10:49:39 +0530

    Time: 540 ms (20.27%), Space: 150.9 MB (10.61%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,29 +1,51 @@
+// O(nLog(n)) => sorting
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length == 0) {
+//             return 0;
+//         }
+//         Arrays.sort(nums);
+        
+        
+//         int longest = 0;
+//         int current =1;
+//         for(int i = 0; i< nums.length -1; i++) {
+//             if(nums[i] == nums[i+1]-1) {
+                
+//                 current++;
+//             } else if(nums[i] == nums[i+1]) {
+                
+//             }
+//             else {
+                
+//                 longest = Math.max(longest, current);
+//                 current = 1;
+//             }
+//         }
+        
+//         longest = Math.max(longest, current);
+        
+//         return longest;
+//     }
+// }
+
+// O(n) => using set;
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
+        public int longestConsecutive(int[] nums) {
+        HashSet set = new HashSet<>();
+        for(int i : nums){
+            set.add(i);
         }
-        Arrays.sort(nums);
-        
-        
-        int longest = 0;
-        int current =1;
-        for(int i = 0; i< nums.length -1; i++) {
-            if(nums[i] == nums[i+1]-1) {
-                
-                current++;
-            } else if(nums[i] == nums[i+1]) {
-                
-            }
-            else {
-                
-                longest = Math.max(longest, current);
-                current = 1;
+        int max = 0;
+        for(int i : nums){
+            if(!set.contains(i-1)){
+                int count = i;
+                while(set.contains(count)){
+                    count++;
+                }
+                max = Math.max(count - i, max);
             }
         }
-        
-        longest = Math.max(longest, current);
-        
-        return longest;
+        return max;
     }
 }

e0513bc0a788536c59be9fdf011f81e5d3a63335
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-09 12:17:53 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (16.21%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -6,13 +6,9 @@ class Solution {
       int peak = 0;
       while(start < end) {
         if(arr[mid] > arr[mid + 1]) {
-
-          //peak = arr[mid] > arr[peak] ? mid : peak;
           end = mid;
-
         } else if(arr[mid] < arr[mid +1]) {
 
-          //peak = arr[mid+1] > arr[peak] ? mid+1 : peak;
           start = mid + 1;
 
         }

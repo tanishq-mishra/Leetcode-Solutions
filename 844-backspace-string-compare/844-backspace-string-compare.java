ec14747eae5acef4cdd214a60f5b466d454de06f
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-09 21:26:43 +0530

    Time: 5 ms (20.58%), Space: 42.3 MB (25.35%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -6,35 +6,18 @@ class Solution {
         for(int i = 0 ; i<s.length(); i++) {
             if(s.charAt(i) != '#') {
                 s1.push(s.charAt(i));
-            } else {
-                if(!s1.empty())
+            } else if(!s1.empty()) {
                     s1.pop();
             }
         }
         for(int i = 0 ; i<t.length(); i++) {
             if(t.charAt(i) != '#') {
                 s2.push(t.charAt(i));
-            } else {
-                if(!s2.empty())
+            } else if(!s2.empty()) {
                     s2.pop();
             }
         }
         
-        while(!s1.empty()) {
-            if(s2.empty())
-                return false;
-            char a = s1.pop();
-            char b = s2.pop();
-            
-            if(a != b) {
-                return false;
-            }
-        }
-        
-        if(!s2.empty()) {
-            return false;
-        }
-        
-        return true;
+        return String.valueOf(s1).equals(String.valueOf(s2));
     }
 }

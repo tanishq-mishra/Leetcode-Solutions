a84ee2aa9d633eaafcd3fb4cc41246c7dd8d3bf8
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-11 22:25:49 +0530

    Time: 7 ms (32.15%), Space: 43.6 MB (7.26%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    String[] numpad ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> helper(String p, String digits) {
+        if(digits.length() == 0) {
+            List<String> ans = new ArrayList<>();
+            ans.add(p);
+            return ans;
+        }
+        int currentDigit = Integer.parseInt(digits.substring(0, 1));
+        List<String> fromBelowCalls = new ArrayList<>();
+        for(int i = 0; i < numpad[currentDigit].length(); i++) {
+            fromBelowCalls.addAll(helper(p+numpad[currentDigit].charAt(i), digits.substring(1)));
+        }
+        
+        return fromBelowCalls;
+    }
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0) {
+            return new ArrayList<>();
+        }
+        return helper("", digits);
+    }
+}

7a9535894dee3a8b349c3b6f8db1109b6f0b8ec3
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-11 22:35:11 +0530

    Time: 10 ms (16.01%), Space: 43.2 MB (14.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,21 +1,53 @@
-class Solution {
-    String[] numpad ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public void helper(String p, int index, String digits, List<String> ans) {
-        if(digits.length() == index) {
-            ans.add(p);
-            return;
-        }
-        int currentDigit = Character.getNumericValue(digits.charAt(index));
-        for(int i = 0; i < numpad[currentDigit].length(); i++) {
-            helper(p+numpad[currentDigit].charAt(i),index+1, digits, ans);
-        }
+// class Solution {
+//     String[] numpad ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//     public void helper(String p, int index, String digits, List<String> ans) {
+//         if(digits.length() == index) {
+//             ans.add(p);
+//             return;
+//         }
+//         int currentDigit = Character.getNumericValue(digits.charAt(index));
+//         for(int i = 0; i < numpad[currentDigit].length(); i++) {
+//             helper(p+numpad[currentDigit].charAt(i),index+1, digits, ans);
+//         }
+//     }
+//     public List<String> letterCombinations(String digits) {
+//         if(digits.length() == 0) {
+//             return new ArrayList<>();
+//         }
+//         List<String> ans = new ArrayList<>();
+//         helper("", 0,digits, ans);
+//         return ans;
+//     }
+// }
+
+
+
+class Solution 
+{
+    String[] help={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) 
+    {
+        List<String> li= new ArrayList<>();
+        if(digits.length()==0)
+            return li;
+        return ListletterCombinations("",digits);
     }
-    public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0) {
-            return new ArrayList<>();
+    public List<String> ListletterCombinations(String p,String up)
+    {
+        if (up.isEmpty())
+        {
+            List<String> list= new ArrayList<>();
+            list.add(p);
+            return list;
         }
-        List<String> ans = new ArrayList<>();
-        helper("", 0,digits, ans);
-        return ans;
+        List<String> list= new ArrayList<>();
+        int digit= up.charAt(0)-'0';
+        String data= help[digit];
+        for (int i = 0; i < data.length(); i++)
+        {
+            char ch=data.charAt(i);
+            list.addAll(ListletterCombinations(p+ch,up.substring(1)));
+        }
+        return list;
     }
 }

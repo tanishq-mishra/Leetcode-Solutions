9789c28c5d3af0700d49c3dfb511caa980eaee70
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-11 14:08:36 +0530

    Time: 53 ms (21.59%), Space: 68.3 MB (9.41%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,33 +1,26 @@
 class Solution {
-    HashMap<List<Integer>, Set<List<Integer>>> memo = new HashMap<>();
-    public Set<List<Integer>> helper(List<Integer> seed, int k) {
-        if(memo.containsKey(seed))
-            return memo.get(seed);
-        if(seed.size() == k) {
-            Set<List<Integer>> ans = new HashSet<>();
-            ans.add(new ArrayList<>(seed));
-            return ans;
-        }
-        Set<List<Integer>> ans = new HashSet<>();
-        for(int i = 0; i<seed.size(); i++) {
-            int temp = seed.get(i);
-            seed.remove(i);
-            ans.addAll(helper(seed, k)); 
-            seed.add(i, temp);
-        }
-        memo.put(seed, ans);
-        return ans;
-    }
+    private List<List<Integer>> res = new LinkedList<>();
+    
     public List<List<Integer>> combine(int n, int k) {
-        List<Integer> seed = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
         
-        for(int i = 1; i<=n; i++) {
-            seed.add(i);
+        dfs(n, k, 1, path);
+        
+        return res;
+    }
+    
+    private void dfs(int n, int k, int start, LinkedList<Integer> path) {
+        if(path.size() == k) {
+            res.add(new LinkedList<>(path));
+            return;
         }
-       
-        List<Integer> ans = new ArrayList<>();
         
-        
-        return new ArrayList<>(helper(seed, k))  ;
+        for(int i = start; i <= n; i++) {
+            path.add(i);
+            
+            dfs(n, k, i + 1, path);
+            
+            path.removeLast();
+        }
     }
 }

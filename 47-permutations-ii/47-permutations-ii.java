a88441b25cb95e8c8362d6ae75a57f13173cc63b
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-12 00:57:35 +0530

    Time: 3 ms (85.05%), Space: 48.4 MB (34.37%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-
-        if (nums == null || nums.length == 0)
-            return new ArrayList<>();
-
-        HashSet<List<Integer>> finalResult = new HashSet<>();
-        permuteRecur(nums, finalResult, new ArrayList<>(), new boolean[nums.length]);
-        return new ArrayList(finalResult);
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(answer, new ArrayList<>(), nums, new boolean[nums.length]);
+        return answer;
     }
-
-    private void permuteRecur(int[] nums, HashSet<List<Integer>> finalResult, List<Integer> currResult, boolean[] used) {
-
-        if (currResult.size() == nums.length) {
-            finalResult.add(new ArrayList<>(currResult));
-            return;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i])
-                continue;
-            currResult.add(nums[i]);
-            used[i] = true;
-            permuteRecur(nums, finalResult, currResult, used);
-            used[i] = false;
-            currResult.remove(currResult.size() - 1);
+    
+    public static void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, boolean[] used){
+        if(tempList.size() == nums.length) 
+            list.add(new ArrayList<>(tempList));
+        else {
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || i > 0 && (nums[i] == nums[i - 1] && !used[i - 1])) 
+                    continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
         }
     }
 }

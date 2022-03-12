b10f3685cffc3107a5f260c21bec71c16f996949
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-12 12:07:12 +0530

    Time: 3 ms (62.45%), Space: 84.2 MB (37.19%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,37 +1,37 @@
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int minPrice = Integer.MAX_VALUE;
-//         int maxProfit = 0;
-        
-//         for(int i = 0 ; i<prices.length; i++) {
-//             if(prices[i] < minPrice) {
-//                 minPrice = prices[i];
-//             } else if((prices[i] - minPrice) > maxProfit) {
-//                 maxProfit = (prices[i] - minPrice);
-//             }
-//         }
-        
-//         return maxProfit;
-//     }
-// }
-
-
 class Solution {
     public int maxProfit(int[] prices) {
-        int left =0;
-        int right = 1;
-        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
         
-        while(right< prices.length) {
-            if(prices[left] < prices[right]) {
-                profit = Math.max(profit, prices[right]-prices[left]);
-                right++;
-            } else {
-                left = right;
-                right++;
+        for(int i = 0 ; i<prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if((prices[i] - minPrice) > maxProfit) {
+                maxProfit = (prices[i] - minPrice);
             }
         }
         
-        return profit;
+        return maxProfit;
     }
 }
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int left =0;
+//         int right = 1;
+//         int profit = 0;
+        
+//         while(right< prices.length) {
+//             if(prices[left] < prices[right]) {
+//                 profit = Math.max(profit, prices[right]-prices[left]);
+//                 right++;
+//             } else {
+//                 left = right;
+//                 right++;
+//             }
+//         }
+        
+//         return profit;
+//     }
+// }

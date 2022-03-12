a3dd86e24e33aa6b242821c6b677cc6a41365dfc
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-12 12:54:06 +0530

    Time: 12 ms (54.42%), Space: 42.3 MB (76.12%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0 || s.length() == 1) {
-            return s.length();
+    public int lengthOfLongestSubstring(String c) {
+        if(c.length() == 0 || c.length() == 1) {
+            return c.length();
         }
         int start = 0;
         int end = 0;
         int maxLength = 0; 
-        char[] c = s.toCharArray();
         
-        while(end < c.length-1) {
+        
+        while(end < c.length()-1) {
             end++;
             int i = end;
-            while( i > start && i > 0 && c[i-1] != c[end]) {
+            while( i > start && i > 0 && c.charAt(i-1) != c.charAt(end)) {
                 i--;
             }
             start = i;
@@ -22,3 +22,32 @@ class Solution {
         return maxLength;
     }
 }
+
+
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s.length() == 0 || s.length() == 1) {
+//             return s.length();
+//         }
+//         int start = 0;
+//         int end = 0;
+//         int maxLength = 0; 
+//         char[] c = s.toCharArray();
+//         HashMap<Character, Integer> set = new HashMap<>();
+//         while(end < c.length-1) {
+//             end++;
+//             if(set.containsKey(c[end])) {
+//                 start = set.get(c[end]) +1;
+                
+//                 set.put(c[end], end);
+                
+//             } else 
+//             start = i;
+//             maxLength = Math.max(maxLength, end-start +1);
+//         }
+        
+        
+//         return maxLength;
+//     }
+// }

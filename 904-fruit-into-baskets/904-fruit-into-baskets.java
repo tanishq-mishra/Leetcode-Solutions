ef2a0a8771b4ea66a7468309d5c6a15c8b4602b4
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-14 15:47:44 +0530

    Time: 26 ms (78.23%), Space: 69.7 MB (75.17%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashSet<Integer> fruitType = new HashSet<>();
+        fruitType.add(fruits[0]);
+        int start = 0;
+        int end = 1;
+        int size = 1;
+        while(end < fruits.length) {
+            //System.out.println("Start = " + start + " End = " + end + " Size = " + size);
+            
+            if(!fruitType.add(fruits[end])) {
+                end++;
+                size = Math.max(size, end-start);
+                continue;
+            } 
+            if(fruitType.size() >2) {
+                int included = fruits[end-1];
+                int i = end-1;
+                while(fruits[i] == included) {
+                    i--;
+                }
+                fruitType.remove(fruits[i]);
+                start = i+1;
+            
+            }
+            
+            
+            
+        }
+        
+        return size;
+    }
+}

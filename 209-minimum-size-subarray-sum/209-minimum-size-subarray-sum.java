4417e605d76daf93accb0f4db07866f683256027
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-14 11:34:46 +0530

    Time: 3 ms (29.42%), Space: 45.7 MB (5.45%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
+        int end = 1;
+        int currentSum = nums[0];
+        int minSize = Integer.MAX_VALUE;
+        while(start < end && end <= nums.length) {
+            //System.out.println(start+", "+ end + " Sum = " + currentSum);
+            if(currentSum >= target) {
+                //System.out.println(end-start);
+                minSize = Math.min(minSize, end-start);
+            } 
+            if(currentSum < target && end != nums.length) {
+                    currentSum +=nums[end];
+                    end++;
+            
+            } else if(currentSum > target) {
+                currentSum -=nums[start];
+                start++;
+            } 
+            else if(end <nums.length) {
+                currentSum +=nums[end];
+                end++;
+            } 
+            
+            else if (end == nums.length){
+                currentSum -=nums[start];
+                start++;
+            } 
+            // else {
+            //     currentSum -=nums[start];
+            //     start++;
+            // }
+            
+            // if(currentSum >= target) {
+            //     //System.out.println(end-start);
+            //     minSize = Math.min(minSize, end-start);
+            // } 
+            
+            // else {
+            //     currentSum +=nums[end];
+            //     end++;
+            // }
+            
+            // if(start>=end) {
+            //     end = start +1;
+            //     currentSum = nums[start];
+            // }
+            
+        }
+        
+        if(minSize == Integer.MAX_VALUE) {
+            return 0;
+        }
+        
+        return minSize;
+    }
+}

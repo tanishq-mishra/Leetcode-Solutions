f2a524f87a2d8c7f250ac49e1451be4cbad69881
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-15 18:18:27 +0530

    Time: 114 ms (22.71%), Space: 99.9 MB (74.76%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,27 +1,47 @@
-class Solution {
+// class Solution {
     
-    public int eraseOverlapIntervals(int[][] intervals) {  
-        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        int counter = 0;
-        boolean removedLast = false;
-        int maxLast = intervals[0][1];
-        for(int i = 0; i < intervals.length -1; i++) {
-            if(intervals[i][1] > intervals[i+1][0]) {
-                if(maxLast > intervals[i+1][0]) {
-                    maxLast = Math.min(intervals[i+1][1], maxLast);
-                    counter++;
-                    removedLast = true;
+//     public int eraseOverlapIntervals(int[][] intervals) {  
+//         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+//         int counter = 0;
+//         boolean removedLast = false;
+//         int maxLast = intervals[0][1];
+//         for(int i = 0; i < intervals.length -1; i++) {
+//             if(intervals[i][1] > intervals[i+1][0]) {
+//                 if(maxLast > intervals[i+1][0]) {
+//                     maxLast = Math.min(intervals[i+1][1], maxLast);
+//                     counter++;
+//                     removedLast = true;
 
-                } else if(removedLast) {
-                    maxLast = intervals[i+1][1];
-                    removedLast = false;
-                }
+//                 } else if(removedLast) {
+//                     maxLast = intervals[i+1][1];
+//                     removedLast = false;
+//                 }
                 
+//             } else {
+//                 maxLast = intervals[i+1][1] ;
+//             }
+//         }
+//         return counter;
+//     }
+// }
+
+
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        
+        int count = 0;
+        int end = intervals[0][0];
+        
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                count++;
             } else {
-                maxLast = intervals[i+1][1] ;
+                end = intervals[i][1];
             }
         }
-        return counter;
+        
+        return count;
     }
 }
-

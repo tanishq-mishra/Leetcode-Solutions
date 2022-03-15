a1dd1033b65c8bf3eefc5352f230cdab208d6c70
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-15 18:14:37 +0530

    Time: 104 ms (33.20%), Space: 100.6 MB (53.69%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -7,13 +7,9 @@ class Solution {
         int maxLast = intervals[0][1];
         int minLast = 0;
         for(int i = 0; i < intervals.length -1; i++) {
-            // System.out.println(intervals[i][0] + " " + intervals[i][1]);
-            // System.out.println(intervals[i+1][0] + " " +intervals[i+1][1]);
-            // System.out.println("maxLast" + maxLast);
             if(intervals[i][1] > intervals[i+1][0]) {
-                System.out.println("First True");
+                //System.out.println("First True");
                 if(maxLast > intervals[i+1][0]) {
-                    //System.out.println("Second True");
                     maxLast = Math.min(intervals[i+1][1], maxLast);
                     counter++;
                     removedLast = true;

3ee4fa7c44c8f8bba7910b2201bfbcd9363037f7
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-15 23:13:22 +0530

    Time: 26 ms (94.25%), Space: 55.5 MB (92.69%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> deque = new LinkedList<Integer>();
+        Deque<Integer> deque = new LinkedList<Integer>();
         int[] ans = new int[nums.length -k +1];
         int i = 0;
         int left = 0;
@@ -8,16 +8,16 @@ class Solution {
         
         while(right < nums.length) {
             
-            while(deque.size() !=0 && nums[deque.get(deque.size()-1)]< nums[right]) {
-                deque.remove(deque.size()-1);
+            while(deque.size() !=0 && nums[deque.peekLast()]< nums[right]) {
+                deque.pollLast();
             }
             deque.add(right);
-            if(left > deque.get(0)) {
-                deque.remove(0);
+            if(left > deque.peekFirst()) {
+                deque.pollFirst();
             }
 
             if(right +1 >= k) {
-                ans[i++] = nums[deque.get(0)];
+                ans[i++] = nums[deque.peekFirst()];
                 right++;
                 left++;
             } else {

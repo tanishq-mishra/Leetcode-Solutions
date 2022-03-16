e3ee06003836dac4e4293052318b1fae9c926ce0
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-16 15:54:03 +0530

    Time: 1 ms (87.17%), Space: 40.9 MB (72.50%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        boolean opposite = true;
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        
+        
+        q.add(root);
+        q.add(null);
+        List<Integer> level = new ArrayList<>();
+        while(!q.isEmpty()) {
+            TreeNode current = q.poll();
+            
+            if(current == null) {
+                opposite = !opposite;
+                if(q.isEmpty()) {
+                    if(opposite) {
+                        Collections.reverse(level);
+                    }
+                    ans.add(new ArrayList<>(level));
+                    break;
+                } else {
+                    if(opposite) {
+                        Collections.reverse(level);
+                    }
+                    ans.add(new ArrayList<>(level));
+                    level.clear();
+                    q.add(null);
+                }
+            } else {
+                
+                level.add(current.val);
+                
+                    if(current.left != null) {
+                        q.add(current.left);
+                    }
+                    if(current.right != null) {
+                        q.add(current.right);
+                    }
+                    
+                
+            }
+        }
+        
+        
+        return ans;
+        
+    }
+}

7398d5fac25a6ad25bd93dc2a81badfeb7c97625
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-22 17:40:36 +0530

    Time: 4 ms (69.57%), Space: 57.5 MB (42.28%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,6 +1,35 @@
 class Solution {
     boolean[][] visited;
     
+//         public void bfs(int i, int j, char[][] grid) {
+//         Queue<int[]> queue = new LinkedList<>();
+//         queue.add(new int[]{i,j});
+        
+//         while(!queue.isEmpty()) {
+//             int[] coords = queue.poll();
+//             int x = coords[0];
+//             int y = coords[1];
+           
+//             visited[x][y] = true;
+//             if(grid[x][y] == '1') {
+//                 if(x+1 <grid.length && !visited[x+1][y] ) {
+//                 queue.add(new int[]{x+1, y});
+//                 }
+//                 if( y+1 < grid[0].length && !visited[x][y+1]) {
+//                     queue.add(new int[]{x, y+1});
+//                 }
+//                 if(x-1 >=0 && !visited[x-1][y] ) {
+//                 queue.add(new int[]{x-1, y});
+//                 }
+//                 if( y-1 >= 0 && !visited[x][y-1]) {
+//                     queue.add(new int[]{x, y-1});
+//                 }
+//             }
+            
+            
+//         }
+//     }
+    
     public void dfs(int i, int j, char[][] grid) {
         if(i >= grid.length || i < 0 || j >= grid[0].length || j < 0) {
             return;

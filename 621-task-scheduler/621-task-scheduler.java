d4b1e9a250e999e14f68b06d1968e47457fea0cf
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-26 22:31:17 +0530

    Time: 47 ms (31.67%), Space: 43.1 MB (92.92%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if(n == 0) {
+            return tasks.length;
+        }
+        Map<Character, Integer> hm = new HashMap<>(); 
+        for(char t : tasks) {
+            hm.put(t, hm.getOrDefault(t, 0) +1);
+        }
+
+         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(Map.Entry<Character, Integer> e : hm.entrySet()) {
+            pq.add(e.getValue());
+        } 
+
+        int time = 0;
+        Queue<List<Integer>> runningQueue = new LinkedList<>();
+
+        while(!pq.isEmpty() || !runningQueue.isEmpty()) {
+            time++;
+            if(!pq.isEmpty()) {
+                int currentTask = pq.poll();
+                
+                currentTask--;
+
+                if(currentTask != 0) {
+                    List<Integer> runningTask = new ArrayList<>();
+                    runningTask.add(currentTask);
+                    runningTask.add(time + n);
+                    runningQueue.add(runningTask);
+                }
+            }
+            if(!runningQueue.isEmpty()) {
+                if(runningQueue.peek().get(1) == time) {
+                    pq.add(runningQueue.poll().get(0));
+                }
+            }
+            
+            
+        }
+        return time;
+    }
+}

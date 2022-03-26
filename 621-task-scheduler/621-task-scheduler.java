9a47052432972c177e40a88e8cc412d53b8e7d34
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-26 22:38:33 +0530

    Time: 86 ms (13.15%), Space: 73.4 MB (5.27%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,14 +1,17 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        // No cool down time, the tasks can run in any sequence witout any idle time.
         if(n == 0) {
             return tasks.length;
         }
+        
+        
         Map<Character, Integer> hm = new HashMap<>(); 
         for(char t : tasks) {
             hm.put(t, hm.getOrDefault(t, 0) +1);
         }
 
-         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         for(Map.Entry<Character, Integer> e : hm.entrySet()) {
             pq.add(e.getValue());

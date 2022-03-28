6c6579229ebfb442494fa8e18d4475342c1c0c58
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-28 16:40:09 +0530

    Time: 40 ms (32.05%), Space: 51 MB (5.73%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -1,15 +1,20 @@
 class NumArray {
     int[] nums;
     int total = 0;
+    Map<String, Integer> sums = new HashMap<>();
 
     public NumArray(int[] nums) {
         this.nums = nums;
         for(int i = 0; i<nums.length; i++) {
             total+= nums[i];
         }
+        sums.put(0 + ","+(nums.length-1), total);
     }
     
     public int sumRange(int left, int right) {
+        if(sums.containsKey(left+","+right)) {
+            return sums.get(left+","+right);
+        }
         int sum = total;
         
         for(int i = 0; i<left; i++) {
@@ -18,7 +23,7 @@ class NumArray {
         for(int i = right+1; i<nums.length; i++) {
             sum-= nums[i];
         }
-        
+        sums.put(left+","+right, sum);
         return sum;
     }
 }

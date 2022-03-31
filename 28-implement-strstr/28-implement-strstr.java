ea0eaa13b560ae21c5141d62491cc0f47a0917a5
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-03-31 23:42:09 +0530

    Time: 2461 ms (7.36%), Space: 42.3 MB (80.32%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        if(needle == "") {
-            return 0;
+    public int strStr(String s, String t) {
+        if (t.isEmpty()) return 0; // edge case: "",""=>0  "a",""=>0
+        for (int i = 0; i <= s.length() - t.length(); i++) {
+            for (int j = 0; j < t.length() && s.charAt(i + j) == t.charAt(j); j++)
+                if (j == t.length() - 1) return i;
         }
-        
-        return haystack.indexOf(needle);
-        
+        return -1;
     }
 }

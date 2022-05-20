2fc5dac8376bd47d6dd28f0723f4ceeb253c4b53
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-20 15:35:33 +0530

    Time: 1 ms (78.95%), Space: 41.6 MB (53.51%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,6 +1,6 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    public void helper(int k, boolean[] used, int n, List<Integer> combi ) {
+    public void helper(int k,  int n, List<Integer> combi ) {
         if(n == 0) {
             if(combi.size() == k) {
                 res.add(new ArrayList<>(combi));
@@ -12,21 +12,16 @@ class Solution {
             return;
         }
         int start = combi.size() == 0 ? 1: combi.get(combi.size()-1)+1;
-        
         for(int i =start ; i<=9 && i<=n; i++) {
-            if(!used[i]) {
-                used[i] = true;
                 combi.add(i);
-                helper(k, used, n-i, combi);
+                helper(k, n-i, combi);
                 combi.remove(combi.size()-1);
-                used[i]=false;
-            }
         }
     }
     
     
     public List<List<Integer>> combinationSum3(int k, int n) {
-        helper(k, new boolean[10], n, new ArrayList<>());
+        helper(k,  n, new ArrayList<>());
         
         return res;
         

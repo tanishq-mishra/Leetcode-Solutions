9f63c5cd3849ffeedc6a9ed3cd375f9f10f0c952
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-20 14:30:57 +0530

    Time: 10 ms (16.33%), Space: 52.1 MB (5.33%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] countBits(int n) {
         int[] res = new int[n+1];
-        
+        HashMap<Integer, Integer> memo = new HashMap<>();
         for(int i = 0; i<res.length;i++) {
             int sum = 0;
             int num = i;
             
             while(num >0) {
+                if(memo.containsKey(num)) {
+                    sum+=memo.get(num);
+                    break;
+                }
                 sum = sum + (num%2);
                 num /=2;
             }
+            memo.put(i, sum);
             res[i] = sum;
         }
         

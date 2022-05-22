7d89c2133b0a9a7cbafca61dae6f53fba383a278
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-22 14:38:03 +0530

    Time: 28 ms (22.13%), Space: 43.3 MB (70.64%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int counter = 0;
+    public HashSet<List<TreeNode>> paths = new HashSet<>();
+    public void helper(TreeNode root, int targetSum, int originalSum) {
+         
+        
+        if(root == null) {
+            return;
+        }
+        
+        if(targetSum-root.val == 0) {
+            counter++;
+        } 
+        
+        if(root == null) {
+            return;
+        }
+        
+
+            
+            helper(root.left,targetSum-root.val,originalSum);
+            helper(root.right, targetSum-root.val,originalSum);
+            
+
+    }
+    
+    
+    public void traverse(TreeNode root, int targetSum) {
+        if(root == null) 
+            return;
+        
+        helper(root, targetSum, targetSum);
+        
+        traverse(root.left, targetSum);
+        traverse(root.right, targetSum);
+        
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        
+        traverse(root, targetSum);
+        
+        
+        return counter;
+    }
+} 

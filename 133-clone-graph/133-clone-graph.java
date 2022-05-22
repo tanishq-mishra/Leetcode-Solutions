50d48a8c65d932fa703239e226538ebd57c78786
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-22 21:54:37 +0530

    Time: 38 ms (44.41%), Space: 43.8 MB (11.15%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.java
@@ -0,0 +1,76 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return node;
+        }
+        
+        if(node.neighbors.size() == 0) {
+            return new Node(node.val);
+        }
+        List<Node> queueOriginal = new ArrayList<>();
+        List<Node> queueCopy = new ArrayList<>();
+        HashSet<Node> visited = new HashSet<>();
+        HashMap<Node, Node> pairs = new HashMap<>();
+        queueOriginal.add(node);
+        while(!queueOriginal.isEmpty()) {
+            Node current = queueOriginal.get(0);
+            queueOriginal.remove(0);
+        
+            if(!visited.contains(current)) {
+                visited.add(current);
+                for(Node neighbor : current.neighbors) {
+                    queueOriginal.add(neighbor);
+                    if(!pairs.containsKey(neighbor)) {
+                        pairs.put(neighbor,new Node(neighbor.val));
+                    }
+                    
+                }
+            }
+        }
+        
+        visited.clear();
+        queueOriginal.add(node);
+        queueCopy.add(pairs.get(node));
+        Node copy = pairs.get(node);
+        while(!queueOriginal.isEmpty()) {
+            Node current = queueOriginal.get(0);
+            queueOriginal.remove(0);
+            
+            Node currentCopy = queueCopy.get(0);
+            queueCopy.remove(0);
+        
+            if(!visited.contains(current)) {
+                visited.add(current);
+                for(Node neighbor : current.neighbors) {
+                    queueOriginal.add(neighbor);
+                    queueCopy.add(pairs.get(neighbor));
+                    currentCopy.neighbors.add(pairs.get(neighbor));
+                }
+            }
+        }
+      
+        
+        
+        return copy;
+    }
+}

bb94ed4031eacae751ff55ce6abaa45fcaa9ba42
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-26 21:40:17 +0530

    Time: 8 ms (44.75%), Space: 43 MB (76.89%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+        
+        for(int i = 0; i<s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+            tCount[t.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i<sCount.length; i++) {
+            if(sCount[i] != tCount[i]) {
+                return false;
+            }
+        }
+        
+        
+        return true;
+        
+    }
+}

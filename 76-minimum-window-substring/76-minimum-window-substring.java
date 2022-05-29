a707661ec905363e3a7569ad93dd5818554e7bc0
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-30 00:58:01 +0530

    Time: 33 ms (23.38%), Space: 47.3 MB (31.46%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,9 +1,5 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()) {
-            return "";
-        }
-        
         if(t.length() == 0) {
             return "";
         }
@@ -13,10 +9,10 @@ class Solution {
         HashMap<Character, Integer> tCount = new HashMap<Character, Integer>();
         
         for(int i = 0; i< t.length(); i++) {
+            // add all character frequencies of t string in a hashMap
             tCount.put(t.charAt(i), tCount.getOrDefault(t.charAt(i), 0) +1);
         }
         
-        //System.out.println(tCount.size());
         
         int left = 0;
         int right = 0;
@@ -25,21 +21,26 @@ class Solution {
         int resLeft = 0;
         int resRight = 0;
         int reSize = Integer.MAX_VALUE;
-        //System.out.println(tCount);
+       
         for(right = 0; right < s.length(); right++) {
+            
             char current = s.charAt(right);
             
             if(tCount.containsKey(current)) {
-                
+                // if the newly added character is present in t then add it to the freq map of s string. 
                 sCount.put(current, sCount.getOrDefault(current, 0)+1);
+                
+                
                 if(sCount.get(current).equals(tCount.get(current))) {
-                    
+                    // if the newly added character frequency equals the freq in the t frequecy map, the increment present counter
+                    // present counter keeps track of all the required frequecy of characters in the window. 
                     present++;
                 }
             }
             
             
-            
+            // present is equal to t freq map size when all the character freq requirements are met i.e, >= freq of characters.
+            // we are incrementing the left pointer to get the smallest possible window which is valid. 
             while(present == tCount.size()) {
                 
                 if(reSize > right-left+1) {

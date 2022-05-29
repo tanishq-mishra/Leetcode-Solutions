31c4f65ca2b77d97f736536ea4e1b0c06f395503
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-29 14:13:18 +0530

    Time: 1 ms (95.01%), Space: 47.9 MB (17.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,60 @@
+// O(n) space and O(n) time
+
+// class Solution {
+//     public int trap(int[] height) {
+//         int[] maxLeft = new int[height.length];
+//         int[] maxRight = new int[height.length];
+        
+//         for(int i = 1; i<height.length; i++) {
+//             maxLeft[i] = Math.max(height[i-1], maxLeft[i-1]);
+//         }
+        
+//         for(int i = height.length-2; i>=0; i--) {
+//             maxRight[i] = Math.max(height[i+1], maxRight[i+1]);
+//         }
+        
+//         int trapped = 0;
+        
+//         for(int i = 0; i< height.length;i++) {
+//             int tempTrapped = Math.min(maxLeft[i], maxRight[i])-height[i];
+            
+//             trapped += tempTrapped > 0 ? tempTrapped : 0;
+//         }
+        
+        
+//         return trapped;
+//     }
+// }
+
+
+//  O(1) space and O(n) time
 class Solution {
     public int trap(int[] height) {
-        int[] maxLeft = new int[height.length];
-        int[] maxRight = new int[height.length];
-        
-        for(int i = 1; i<height.length; i++) {
-            maxLeft[i] = Math.max(height[i-1], maxLeft[i-1]);
-        }
-        
-        for(int i = height.length-2; i>=0; i--) {
-            maxRight[i] = Math.max(height[i+1], maxRight[i+1]);
-        }
-        
+        int maxLeft = 0;
+        int maxRight = 0;
+        int left = 0;
+        int right = height.length-1;
         int trapped = 0;
         
-        for(int i = 0; i< height.length;i++) {
-            int tempTrapped = Math.min(maxLeft[i], maxRight[i])-height[i];
-            
-            trapped += tempTrapped > 0 ? tempTrapped : 0;
+        while(left <= right) {
+            if(maxLeft < maxRight) {
+                int tempTrapped = (maxLeft) - height[left];
+                trapped += tempTrapped >0 ? tempTrapped : 0;
+                
+                maxLeft = Math.max(maxLeft, height[left]);
+                left++;
+                
+            } else {
+                int tempTrapped = maxRight - height[right];
+                trapped += tempTrapped >0 ? tempTrapped : 0;
+                maxRight = Math.max(maxRight, height[right]);
+                right--;
+            }
         }
         
         
         return trapped;
+        
+        
     }
 }

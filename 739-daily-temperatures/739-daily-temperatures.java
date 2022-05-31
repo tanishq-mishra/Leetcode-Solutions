fe32070cf4ddc084c267b85054b68070a5d956e6
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-05-31 22:27:27 +0530

    Time: 49 ms (74.61%), Space: 59.3 MB (71.88%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -4,7 +4,7 @@ class Solution {
         int index;
         int value;
         
-        StackEntry(int i, int v) {
+        StackEntry(int i,int v) {
             index = i;
             value = v;
         }
@@ -13,17 +13,18 @@ class Solution {
     
     public int[] dailyTemperatures(int[] temperatures) {
         int[] res = new int[temperatures.length];
-        Stack<StackEntry> stack = new Stack<>();
+        List<List<Integer>> stack = new ArrayList<>();
         
         for(int i = 0; i<temperatures.length; i++) {
-            while(stack.size() != 0 && stack.peek().value < temperatures[i]) {
-                StackEntry temp = stack.pop();
-                res[temp.index] = i - temp.index;
-                
+            while(stack.size() != 0 && stack.get(stack.size()-1).get(1) < temperatures[i]) {
+                List<Integer> temp = stack.get(stack.size()-1);
+                res[temp.get(0)] = i - temp.get(0);
+                stack.remove(stack.size()-1);
             }
-            StackEntry newTemp = new StackEntry(i, temperatures[i]);
-            
-            stack.push(newTemp);
+            List<Integer> newTemp = new ArrayList<Integer>();
+            newTemp.add(i);
+            newTemp.add(temperatures[i]);
+            stack.add(newTemp);
         }
         
         

9c745fb55df7f3ac8b9aa43dd90afef489bb00cd
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-06-05 00:51:56 +0530

    Time: 96 ms (30.18%), Space: 132.6 MB (22.42%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.java
@@ -0,0 +1,44 @@
+
+
+class LRUCache {
+    int capacity;
+    int current;
+    Map<Integer, Integer> list;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        current = 0;
+        list = new LinkedHashMap<>(){
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest)
+            {
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        
+        Integer value = list.get(key);
+        if(value == null) {
+            return -1;
+        }
+        list.remove(key);
+        list.put(key, value);
+        
+        return value;
+    }
+    
+    public void put(int key, int value) {
+        
+        if(list.containsKey(key)) {
+            list.remove(key);
+        }
+        list.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */

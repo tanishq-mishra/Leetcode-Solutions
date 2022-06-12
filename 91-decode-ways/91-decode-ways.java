e45187c87751c3aa94db3bee9ab4908fc0abda71
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-06-13 00:45:28 +0530

    Time: 3 ms (34.91%), Space: 42.9 MB (17.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,40 +1,40 @@
 class Solution {
-    HashMap<String, Integer> memo = new HashMap<>();
-    public int helper(String s) {
-        if(memo.containsKey(s)) {
-            return memo.get(s);
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    public int helper(String s, int current) {
+        if(memo.containsKey(current)) {
+            return memo.get(current);
         }
-        if(s.length() == 1 && s.charAt(0) != '0') {
+        if(current == s.length()-1 && s.charAt(current) != '0') {
             return 1;
         }
-        if(s.length() == 0) {
+        if(current == s.length()) {
             return 1;
         }
-        if(s.charAt(0) == '0') {
+        if(s.charAt(current) == '0') {
             return 0;
         }
         
-        int value = Integer.parseInt(s.substring(0, 2));
+        int value = Integer.parseInt(s.substring(current, current+2));
         
         if(value % 10 == 0 && value <=20) {
-            memo.put(s, helper(s.substring(2)));
-            return memo.get(s);
+            memo.put(current, helper(s, current+2));
+            return memo.get(current);
         }
         
         if(value <=26) {
-            memo.put(s, helper(s.substring(1))+ helper(s.substring(2)));
-            return memo.get(s);
+            memo.put(current, helper(s, current+1)+ helper(s, current+2));
+            return memo.get(current);
             
         } else {
-            memo.put(s, helper(s.substring(1)));
-            return memo.get(s);
+            memo.put(current, helper(s, current+1));
+            return memo.get(current);
         }
     }
     public int numDecodings(String s) {
         if(s.charAt(0) == '0') {
             return 0;
         }
-         return helper(s);
+         return helper(s, 0);
         
     }
 }

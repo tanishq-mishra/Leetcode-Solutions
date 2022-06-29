fa7556cb4b1910dd56198d56c2be4eec7ce1d183
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-06-30 02:01:40 +0530

    Time: 7 ms (8.15%), Space: 42.3 MB (49.68%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,27 @@
+class Solution { 
+    
+    public int helper(int[][] obstacleGrid, int i, int j, int[][] memo) {
+        if(i<0 || j<0 || obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+        if(memo[i][j] != 0) {
+            return memo[i][j];
+        }
+        if(i == 0  && j == 0) {
+            return 1;
+        }
+        
+        
+        int left = helper(obstacleGrid, i-1, j,memo);
+        int up = helper(obstacleGrid, i, j-1,memo);
+        memo[i][j] = left+up;
+        return memo[i][j];
+    }
+    
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] memo = new int[obstacleGrid.length][obstacleGrid[0].length];
+        //Arrays.fill(memo, -1);
+        return helper(obstacleGrid, obstacleGrid.length-1,obstacleGrid[0].length-1, memo);
+    }
+}

7d8dc491e59460b7402f69ba45a86115e30d59d4
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-07-03 22:14:33 +0530

    Time: 6 ms (43.50%), Space: 44.2 MB (42.51%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,26 +1,54 @@
 class Solution {
-    public int helper(List<List<Integer>> triangle, int i, int j, int[][] memo) {
-        if(memo[i][j] != Integer.MAX_VALUE) {
-            return memo[i][j];
-        }
-        if(i == triangle.size()-1) {
-            return triangle.get(i).get(j);
-        }
+    
+    // Top-Down DP with Memo
+//     public int helper(List<List<Integer>> triangle, int i, int j, int[][] memo) {
+//         if(memo[i][j] != Integer.MAX_VALUE) {
+//             return memo[i][j];
+//         }
+//         if(i == triangle.size()-1) {
+//             return triangle.get(i).get(j);
+//         }
         
-        int down = triangle.get(i).get(j) + helper(triangle, i+1, j, memo);
-        int diagonal  = triangle.get(i).get(j) + helper(triangle, i+1, j+1, memo);
-        memo[i][j] = Math.min(down, diagonal);
-        return memo[i][j];
+//         int down = triangle.get(i).get(j) + helper(triangle, i+1, j, memo);
+//         int diagonal  = triangle.get(i).get(j) + helper(triangle, i+1, j+1, memo);
+//         memo[i][j] = Math.min(down, diagonal);
+//         return memo[i][j];
         
         
-    }
+//     }
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int[][] memo = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+        
+//         for(int[] m : memo) {
+//             Arrays.fill(m, Integer.MAX_VALUE);
+//         }
+        
+//         return helper(triangle, 0, 0, memo);
+//     }
+    
+    // Bottom-Up Conversion
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] memo = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+        int[][] dp = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+         
+        int m = triangle.size();
+        int n = triangle.get(triangle.size()-1).size();
         
-        for(int[] m : memo) {
-            Arrays.fill(m, Integer.MAX_VALUE);
+       
+        
+        for(int i = m-1; i>=0; i--) {
+            for(int j = triangle.get(i).size()-1; j>=0; j--) {
+                if(i == m-1) {
+                    dp[i][j] = triangle.get(i).get(j);
+                } else {
+                    int down = triangle.get(i).get(j) + dp[i+1][j];
+                    int diagonal  = triangle.get(i).get(j) + dp[i+1][j+1];
+                    dp[i][j] = Math.min(down, diagonal);
+                }
+            }
         }
-        
-        return helper(triangle, 0, 0, memo);
+        return dp[0][0];
     }
+    
+    
+    
 }

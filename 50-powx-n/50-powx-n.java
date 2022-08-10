655ce6df58825910e851975ce327153d10c6465b
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-10 19:49:13 +0530

    Time: 1 ms (89.32%), Space: 42.7 MB (60.22%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // binary Exponentiation
+        long nn = n;
+        double ans = 1.0;
+        if(nn <0) {
+            nn *= -1;
+        }
+        while(nn>0) {
+            if(nn % 2 == 1) {
+                ans = ans * x;
+                nn-=1;
+            } else {
+                x = x*x;
+                nn/=2;
+            }
+        }
+        
+        if(n <0) {
+            ans = 1/ans;
+        }
+        
+        return ans;
+    }
+}

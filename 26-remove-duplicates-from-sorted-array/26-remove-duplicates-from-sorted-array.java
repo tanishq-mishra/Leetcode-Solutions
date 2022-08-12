eaa73979cad8f051dc16f892f5797a2c767b1930
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-12 20:45:35 +0530

    Time: 1 ms (100.00%), Space: 47.9 MB (48.87%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,21 +1,13 @@
 class Solution {
-    public void shift (int[] nums, int start, int end) {
-         for(int i = start; i<end-1; i++) {
-             nums[i] = nums[i+1];
-         }
-        
-        
-    }
     public int removeDuplicates(int[] nums) {
         int dups = 0;
         int i = 0;
-        while(i<nums.length-dups-1) {
-            if(nums[i] == nums[i+1]) {
-                shift(nums, i, nums.length-dups);
+        for(i = 1; i<nums.length; i++) {
+            if(nums[i-1] == nums[i]) {
                 dups++;
-            } else {
-                i++;
             }
+            
+            nums[i-dups] = nums[i];
         }
         
         

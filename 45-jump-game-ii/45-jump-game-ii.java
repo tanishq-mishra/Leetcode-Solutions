f732fbc9fd38370303f2f855383595b6017bd188
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-17 13:21:31 +0530

    Time: 41 ms (41.07%), Space: 49.5 MB (44.51%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -29,7 +29,6 @@ class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
         
-        dp[nums.length-1] = 0;
         
         for ( int i = nums.length-2; i>=0; i--) {
             

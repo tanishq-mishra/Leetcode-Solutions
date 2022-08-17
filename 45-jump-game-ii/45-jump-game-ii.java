1d358ae267f122bec9ecee593bf6e5f3051c8526
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-17 13:21:08 +0530

    Time: 80 ms (28.20%), Space: 48.7 MB (82.53%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,27 +1,46 @@
 class Solution {
-    
-    public int helper(int[] nums, int idx, int[] memo) {
-        if(idx == nums.length-1) {
-            return 0;
-        }
-        if(memo[idx] != -1) {
-            return memo[idx];
-        }
-        if(nums[idx] == 0) {
-            return 1000000;
-        }
+    // Memoization
+//     public int helper(int[] nums, int idx, int[] memo) {
+//         if(idx == nums.length-1) {
+//             return 0;
+//         }
+//         if(memo[idx] != -1) {
+//             return memo[idx];
+//         }
+//         if(nums[idx] == 0) {
+//             return 1000000;
+//         }
         
-        int min = Integer.MAX_VALUE;
-        for(int i = 1; i<=nums[idx] && i+idx< nums.length;i++) {
+//         int min = Integer.MAX_VALUE;
+//         for(int i = 1; i<=nums[idx] && i+idx< nums.length;i++) {
             
-            min = Math.min(helper(nums, idx+i, memo)+1, min);
-        }
-        memo[idx] = min;
-        return min;
-    }
+//             min = Math.min(helper(nums, idx+i, memo)+1, min);
+//         }
+//         memo[idx] = min;
+//         return min;
+//     }
+//     public int jump(int[] nums) {
+//         int[] memo = new int[nums.length];
+//         Arrays.fill(memo, -1);
+//         return helper(nums, 0, memo);
+//     }
+    
+    // converted tabulation
     public int jump(int[] nums) {
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return helper(nums, 0, memo);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 0);
+        dp[nums.length-1] = 0;
+        
+        for ( int i = nums.length-2; i>=0; i--) {
+            
+            int min = 1000000;
+            for(int j = 1; j<=nums[i] && j+i< nums.length;j++) {
+
+                min = Math.min(dp[j+i]+1, min);
+            }
+            dp[i] = min;
+            
+        }
+        return dp[0];
     }
 }

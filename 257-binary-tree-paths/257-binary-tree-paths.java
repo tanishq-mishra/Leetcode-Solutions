f27e9c8c215c045e0ea2b3975d7c4f0e4f15f4e5
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-21 23:43:24 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (91.45%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -27,7 +27,7 @@ class Solution {
             res.add(path);
             return;
         }
-        path += String.valueOf(root.val);
+        path += Integer.toString(root.val);
         path +="->";
         helper(root.left, path);
         helper(root.right, path);

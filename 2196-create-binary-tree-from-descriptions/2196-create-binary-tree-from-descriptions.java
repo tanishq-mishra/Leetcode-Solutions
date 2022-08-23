653ebb391d6f80ba1a3448a4c7e445d3a9294ba9
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-24 01:43:31 +0530

    Time: 123 ms (62.91%), Space: 118.4 MB (75.18%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> nodes = new HashMap<>();
+        
+        for(int[] d : descriptions) {
+            TreeNode parent = nodes.getOrDefault(d[0], new TreeNode());
+            parent.val = d[0];
+            TreeNode child = nodes.getOrDefault(d[1], new TreeNode());
+            child.val = d[1];
+            if(d[2] == 1) {
+                parent.left = child;
+                
+            } else {
+               parent.right = child;
+                
+            }
+            nodes.put(d[1], child);
+            nodes.put(d[0], parent);
+            
+            
+        }
+        for(int[] d : descriptions) {
+            nodes.remove(d[1]);
+        }
+        TreeNode root = null;
+        for(int node : nodes.keySet()) {
+            root = nodes.get(node);
+            break;
+        }
+        
+        
+        return root;
+    }
+}

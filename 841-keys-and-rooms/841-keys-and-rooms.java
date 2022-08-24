3876baf4a4c102cff544c8bf0e04670bbbe8fa04
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-25 01:37:49 +0530

    Time: 5 ms (23.54%), Space: 46.3 MB (5.59%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        List<Integer> keys = new ArrayList<>();
+        keys.addAll(rooms.get(0));
+        HashSet<Integer> usedKeys = new HashSet<>();
+        
+        usedKeys.add(0);
+        
+        
+        while(!keys.isEmpty()) {
+            List<Integer> newKeys = new ArrayList<>();
+            while(!keys.isEmpty()) {
+                int key = keys.get(0);
+                keys.remove(0);
+                usedKeys.add(key);
+                
+                newKeys.addAll(rooms.get(key));
+            }
+            
+            for(int key : newKeys) {
+                if(!usedKeys.contains(key)) {
+                    keys.add(key);
+                }
+            }
+            
+            
+        }
+            
+        
+        
+        
+        return (usedKeys.size() == rooms.size());
+    }
+}

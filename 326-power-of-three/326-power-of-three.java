3fe16bdd6af17978c6d5a77ffd898b00a6f7bdd2
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-24 14:08:41 +0530

    Time: 15 ms (98.30%), Space: 42.3 MB (86.45%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,22 +1,29 @@
 class Solution {
-    public boolean isPowerOfThree(int n) {
+    // log(n);
+//     public boolean isPowerOfThree(int n) {
         
-        if(n <= 0) {
-            return false;
-        }
-        if(n == 1) {
-            return true;
-        }
-        while(n>1) {
-            double d = (double)n/3;
+//         if(n <= 0) {
+//             return false;
+//         }
+//         if(n == 1) {
+//             return true;
+//         }
+//         while(n>1) {
+//             double d = (double)n/3;
             
-            if(Math.floor(d) != d) {
-                return false;
-            }
-            n = (int)d;
-        }
+//             if(Math.floor(d) != d) {
+//                 return false;
+//             }
+//             n = (int)d;
+//         }
         
-        return true;
+//         return true;
         
+//     }
+    
+    // constant time;
+    public boolean isPowerOfThree(int n) {
+        // 1162261467 is 3^19,  3^20 is bigger than int  
+        return ( n>0 &&  1162261467%n==0);
     }
 }

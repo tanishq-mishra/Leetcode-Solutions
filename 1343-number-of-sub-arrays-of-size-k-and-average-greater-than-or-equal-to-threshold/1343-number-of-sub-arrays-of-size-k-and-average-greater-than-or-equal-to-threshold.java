61931211d4be1422dda92802ff4a4edb2539f49c
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-25 13:33:58 +0530

    Time: 4 ms (78.36%), Space: 69.8 MB (38.11%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,9 +1,12 @@
 class Solution {
+    // Sliding window
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int start = 0;
         int end = -1;
         int sum = 0;
         int count = 0;
+        
+        // initial Window
         while(end<k-1) {
             sum+=arr[++end];
         }
@@ -15,7 +18,7 @@ class Solution {
             sum-= arr[start++];
             sum+= arr[++end];
         }
-        
+        // considering the last window
         if(sum/k >= threshold) {
                 count++;
             }

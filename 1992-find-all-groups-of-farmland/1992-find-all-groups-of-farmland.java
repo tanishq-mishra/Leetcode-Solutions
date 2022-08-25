cfbbff0a685163a029dd0f308eeb4c7654d5590f
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-26 01:47:50 +0530

    Time: 39 ms (27.19%), Space: 144.2 MB (7.89%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,58 @@
+class Solution {
+    int x1, y1, x2, y2;
+    
+    public void helper(int[][] land, boolean[][] visited, int i, int j) {
+        if(i <0 || j < 0 || i>=land.length || j >= land[0].length || visited[i][j] || land[i][j] == 0) {
+            return;
+        }
+        
+        visited[i][j] = true;
+        x1 = Math.min(x1,i);
+        y1 = Math.min(y1, j);
+        x2 = Math.max(x2, i);
+        y2 = Math.max(y2, j);
+        
+        
+        helper(land, visited, i+1, j);
+        helper(land, visited, i, j+1);
+        helper(land, visited, i-1, j);
+        helper(land, visited, i, j-1);
+
+    }
+    
+    
+    public int[][] findFarmland(int[][] land) {
+        boolean[][] visited = new boolean[land.length][land[0].length];
+        List<int[]> res = new ArrayList<>();
+        for(int i = 0; i<land.length; i++) {
+            for(int j = 0; j<land[0].length; j++) {
+                if(land[i][j] == 1 && !visited[i][j]) {
+                    x1 = i;
+                    y1 = j;
+                    x2 = i;
+                    y2 = j;
+                    helper(land, visited, i, j);
+                    res.add(new int[]{x1,y1,x2,y2});
+                }
+            }
+        }
+        
+        int[][] result = new int[res.size()][4];
+        int i = 0;
+        for(int[] r : res) {
+            result[i][0] = r[0];
+            result[i][1] = r[1];
+            result[i][2] = r[2];
+            result[i][3] = r[3];
+            i++;
+        }
+        
+        return result;
+    }
+}
+
+
+// /* 0 1
+//    1 0
+   
+   

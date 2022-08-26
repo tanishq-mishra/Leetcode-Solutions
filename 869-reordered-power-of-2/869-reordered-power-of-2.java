360e9ec84db69398fa92b8e7be6d7cf474c95dcf
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-26 15:57:25 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (46.67%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,51 +1,13 @@
 class Solution {
-    public boolean isValid(int n) {
-        
-        if(n ==0) {
-            return false;
-        }
-        if(n == 1) {
-            return true;
-        }
-        while(n>1 && n% 2 == 0) {
-            n/=2; 
-        }
-        
-        return n == 1;
-    }
-    
-    public boolean helper(ArrayList<Integer> digits, int current, boolean[] visited, int count) {
-        if( count == digits.size()) {
-            return isValid(current);
-        }
-        
-        for(int i = 0; i< digits.size(); i++) {
-            if(current == 0 && digits.get(i) == 0 || visited[i]) {
-                continue;
-            }
-            
-            
-            visited[i] = true;
-            //System.out.println(current);
-            if(helper(digits, (current*10) + digits.get(i), visited, count+1)) {
-                return true;
-            }
-            visited[i] = false;
-        }
-        
+    public boolean reorderedPowerOf2(int N) {
+        long c = counter(N);
+        for (int i = 0; i < 32; i++)
+            if (counter(1 << i) == c) return true;
         return false;
     }
-    public boolean reorderedPowerOf2(int n) {
-        ArrayList<Integer> digits = new ArrayList<>();
-        int val = n ;
-        while(val > 0) {
-            digits.add(val%10);
-            val/=10;
-        }
-        //System.out.println(digits);
-        
-        
-        
-        return helper(digits, 0, new boolean[digits.size()], 0);
+    public long counter(int N) {
+        long res = 0;
+        for (; N > 0; N /= 10) res += (int)Math.pow(10, N % 10);
+        return res;
     }
-} 
+}

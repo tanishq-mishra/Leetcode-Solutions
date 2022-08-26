9d123b896dec843d1ef08a82d6408fb0d5fdfc7e
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-26 16:00:54 +0530

    Time: 2 ms (68.67%), Space: 39.4 MB (90.00%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean reorderedPowerOf2(int N) {
-        long c = counter(N);
-        for (int i = 0; i < 32; i++)
-            if (counter(1 << i) == c) return true;
+        char[] a1 = String.valueOf(N).toCharArray();
+        Arrays.sort(a1);
+        String s1 = new String(a1);
+        
+        for (int i = 0; i < 31; i++) {
+            char[] a2 = String.valueOf((int)(1 << i)).toCharArray();
+            Arrays.sort(a2);
+            String s2 = new String(a2);
+            if (s1.equals(s2)) return true;
+        }
+        
         return false;
-    }
-    public long counter(int N) {
-        long res = 0;
-        for (; N > 0; N /= 10) res += (int)Math.pow(10, N % 10);
-        return res;
     }
 }

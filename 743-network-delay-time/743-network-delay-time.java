17d956f554d2c5dcaa66ee145fd737de99df6557
Tanishq Mishra <56171381+tanishq-mishra@users.noreply.github.com>
2022-08-28 13:40:15 +0530

    Time: 15 ms (88.96%), Space: 46.7 MB (88.36%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -8,32 +8,23 @@ class Solution {
         // for(int i = 0; i<costs.length; i++) {
         //     System.out.print(costs[i] + "  ");
         // }
-        for(int i = 0; i<n; i++) {
+        for(int i = 0; i<n-1; i++) {
             
             
             for(int[] t: times) {
                 int source = t[0]-1;
                 int destination = t[1]-1;
                 int cost = t[2];
-                // if(costs[source] == Integer.MAX_VALUE)
-                //     continue;
                 costs[destination] = Math.min(costs[destination], costs[source] + cost);
             }
-            // costs = temp;
-            // for(int j= 0; j<costs.length; j++) {
-            // System.out.print(costs[j] + "  ");
-            // }
-            // System.out.println();
+
             
         }
         
         int max = -100000000;
-        for(int i = 0; i<n; i++) {
-            //System.out.print(costs[i] + "  ");
-            
+        for(int i = 0; i<n; i++) { 
             max = Math.max(max, costs[i]);
         }
-        //System.out.println();
         
         return max == 100000000? -1 : max;
     }
